8ceaf76f45d222fd9f892d164bca13d3270b99a0
sudu <teric@outlook.com>
2025-12-05 18:45:39 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,15 @@
 import { serveFile } from "jsr:@std/http/file-server";
+import { Sandbox } from "@deno/sandbox";
+
+await using sandbox = await Sandbox.create();
+
+// await sandbox.writeTextFile("hello.ts", "console.log('Hello, Sandbox!');");
+// const text = await sandbox.sh`pwd`.text();
+const result = await sandbox.eval(`
+  const a = 1;
+  const b = 2;
+  a + b;
+`);
 
 Deno.serve((req: Request) => {
     return serveFile(req, "./index.html");
